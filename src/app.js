9de9b4c53d18b1b6d1bc96ab2eb47e5bbfa7dacb
gonzalo-novak <gonzalo.novak@monstertrap.art>
2021-09-19 23:36:46 -0500

    chore: rudimentary config to add routes compatibility

--- src/app.js
+++ src/app.js
@@ -6,5 +6,10 @@ const app = () => {
 	const jsTestElement = document.getElementById('js-test');
 	jsTestElement.classList.add('success');
 	jsTestElement.innerText = 'Ready!';
+
+	const clickToContinueButton = document.getElementById('click-to-continue');
+	clickToContinueButton.onclick = () => {
+		window.location.pathname = '/pages/login.html';
+	};
 };
 app();
