eb1b922e98358d5db87b2449404b07b875ac7b58
AlanPinhon <pinhoneibor32@gmail.com>
2021-09-21 17:12:35 -0500

    chore(registro): maquetación y estilos

--- src/app.js
+++ src/app.js
@@ -1,15 +1,2 @@
 //Loading styles
 import './styles/app.scss';
-
-//Firing our whole app
-const app = () => {
-	const jsTestElement = document.getElementById('js-test');
-	jsTestElement.classList.add('success');
-	jsTestElement.innerText = 'Ready!';
-
-	const clickToContinueButton = document.getElementById('click-to-continue');
-	clickToContinueButton.onclick = () => {
-		window.location.pathname = '/pages/login.html';
-	};
-};
-app();
