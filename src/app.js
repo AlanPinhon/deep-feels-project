e30ff092b8d0f7983739ba40414c4c1e2697e20f
Alan Piñón <pinhoneibor32@gmail.com>
2021-10-25 18:15:40 -0600

    chore(email): maquetación de pantalla para recuperar contraseña

--- src/app.js
+++ src/app.js
@@ -2,3 +2,4 @@
 import './styles/app.scss';
 import './styles/user-mood.scss';
 import './styles/forgot-pass.scss';
+import './styles/reset-pass-email.scss';
--- src/js/emailSent.js
+++ src/js/emailSent.js
@@ -1,12 +1,5 @@
-import { redirect } from "./redirect";
-
 const resendEmail = document.querySelector('.resend');
 const fwdEmail = document.querySelector('.msg-fwd-email');
-const linkToLogin = document.querySelector('.login');
-
-linkToLogin.addEventListener('click', () => {
-	redirect('login');
-});
 
 resendEmail.addEventListener('click', () => {
 	fwdEmail.classList.add('active');
