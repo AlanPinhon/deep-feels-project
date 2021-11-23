fdde3f23eabeb6ea49bc29451a983cbb7b01d945
Alan Piñón <pinhoneibor32@gmail.com>
2021-11-23 15:47:02 -0600

    chore(profile): previsualización de foto al cargarla y cambio de datos del perfil

--- src/js/profileSettings.js
+++ src/js/profileSettings.js
@@ -8,12 +8,16 @@ const inputNameChange = document.querySelector("#name");
 const inputEmailChange = document.querySelector("#email");
 const saveBtn = document.querySelector('#save');
 const cancelBtn = document.querySelector('#cancel');
+const uploadImg = document.querySelector('#file-up');
+const img = document.querySelector('.preview');
 
+// Función que llena los inputs del usuario actual
 const fillInputs = () => {
 	inputNameChange.value = currentUser().name;
 	inputEmailChange.value = currentUser().email;
 };
 
+// Validación de inputs
 const inputs = {
 	name: false,
 	email: false
@@ -24,6 +28,11 @@ const validateInputs = (e) => {
 };
 
 const validateInput = (expresion, input, message) => {
+
+	if(input !== ""){
+		saveBtn.style.display = 'block';
+		cancelBtn.style.display = 'block';
+	}
 
 	if(!expresion.test(input.value)){
 		input.classList.remove('active');
@@ -53,18 +62,43 @@ const profileListeners = () => {
 	inputNameChange.addEventListener('input', validateInputs);
 	inputEmailChange.addEventListener('input', validateInputs);
 
+	// Actualiza los datos del usuario.
 	profileForm.addEventListener('submit', (e) => {
 		e.preventDefault();
 		console.log('Datos guardados con éxito');
+		// Oculta los botones "Guardar cambios" y "Cancelar"
 		saveBtn.classList.remove('button-active');
 		saveBtn.disabled = true;
+		saveBtn.style.display = 'none';
+		cancelBtn.style.display = 'none';
 	});
 
 	backHome.addEventListener('click', () => {
 		redirect('in-app');
 	});
 
+	// Carga el preview de la imagen
+	uploadImg.addEventListener('change', (e) => {
+		img.classList.add('change');
+		img.src = URL.createObjectURL(e.target.files[0]);
+		saveBtn.style.display = 'block';
+		saveBtn.classList.add('button-active');
+		saveBtn.disabled = false;
+		cancelBtn.style.display = 'block';
+	});
+
+	// Regresa a los valores iniciales del perfil
 	cancelBtn.addEventListener('click', () => {
+		uploadImg.value = '';
+		img.classList.remove('change');
+		img.src =
+		'../../pages/deep_feels_assets/settings_profile/bxs-user.svg';
+		saveBtn.style.display = 'none';
+		cancelBtn.style.display = 'none';
+		saveBtn.classList.remove('button-active');
+		saveBtn.disabled = true;
+		inputNameChange.classList.remove('active');
+		inputEmailChange.classList.remove('active');
 		fillInputs();
 	});
 };
